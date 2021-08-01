9b1070ffa9edf1bf50055b572d41ae987b3eeac6
Jeongbin Park <pjb7687@gmail.com>
2021-08-01 14:38:39 +0200

    fix dll dependency search

--- scripts/build_gdl.sh
+++ scripts/build_gdl.sh
@@ -331,7 +331,7 @@ function prep_packages_dryrun {
 }
 
 function find_dlls {
-    for dll in $(strings $1 | grep -i '[a-zA-Z0-9]\.dll$' | grep -v " " | grep -v $(basename $1)); do
+    for dll in $(strings $1 | grep -i '[a-zA-Z0-9]\.dll' | grep -v " " | grep -v $(basename $1)); do
         dll="/mingw64/bin/$dll"
         if [ -f "$dll" ] && [[ ! ${found_dlls[@]} =~ (^|[[:space:]])"$dll"($|[[:space:]]) ]]; then
             log "Found DLL dependency: $dll"
@@ -437,7 +437,7 @@ function install_gdl {
         for f in ${found_dlls[@]}; do
             cp -f "$f" bin/
         done
-        
+
         log "Copying plplot drivers to install directory..."
         mkdir -p share
         cp -rf /${mname}/share/plplot* share/
